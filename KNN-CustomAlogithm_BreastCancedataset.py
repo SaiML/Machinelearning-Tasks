64c56f254ad15e44253457e7d797c329df5e7adf
Sainath Reddy <54442449+SaiML@users.noreply.github.com>
2019-09-20 11:47:28 +0530

    Update KNN-CustomAlogithm_BreastCancedataset.py

--- KNN-CustomAlogithm_BreastCancedataset.py
+++ KNN-CustomAlogithm_BreastCancedataset.py
@@ -5,112 +5,54 @@ Created on Wed Sep  4 16:28:16 2019
 @author: Sainath.Reddy
 """
 
-# =============================================================================
-# Importing Libraries
-# =============================================================================
 import pandas as pd
 import numpy as np
-from math import sqrt
-import warnings
 from collections import Counter
 import matplotlib.pyplot as plt
-import seaborn as sns
-import random
-import io
-from sklearn import metrics
+import random,time,warnings
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from imblearn.over_sampling import SMOTE 
 from sklearn.metrics import classification_report,confusion_matrix
-from functools import wraps
-from time import time
-import time
-import warnings
+from sklearn import metrics
 warnings.filterwarnings('ignore')
 
-# =============================================================================
-# Loading Dataset
-# =============================================================================
-df=pd.read_csv("breast-cancer-wisconsin.data", header=None)
-print(df.head(),'\n Shape of DataFrame is ',df.shape)
 
-# =============================================================================
-#  We Do not have any information regarding the attributes of the dataset so tried to fetch the information 
-# from Machine learning Repository
-# Reference link: https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Original)
-# """ Relavant information Regarding Dataset  
-#   Number of Attributes: 10 plus the class attribute
-#    #  Attribute                     Domain
-#    -- -----------------------------------------
-#    1. Sample code number            id number
-#    2. Clump Thickness               1 - 10
-#    3. Uniformity of Cell Size       1 - 10
-#    4. Uniformity of Cell Shape      1 - 10
-#    5. Marginal Adhesion             1 - 10
-#    6. Single Epithelial Cell Size   1 - 10
-#    7. Bare Nuclei                   1 - 10
-#    8. Bland Chromatin               1 - 10
-#    9. Normal Nucleoli               1 - 10
-#   10. Mitoses                       1 - 10
-#   11. Class:                        (2 for benign, 4 for malignant) 
-
-# =============================================================================
-# EDA
-# =============================================================================
-
-df.columns = ['id','Clump_thickness','Uniformity_of_cellsize','uniformity_of_cellshape','Marginal_adhesion','Single_epithelain_cellsize',
-        'Bare_nuclei','Bland_chromatin','Nornal_Nucleli','Mitoses','Class']
-print('head of DF',df.head(),'\n Info about DF',df.info(),'\n Description About DF',df.describe().transpose())
-
-#Checking for Null Values
-print('Null values',df.isnull().sum(),'\n unique values',df.nunique())
-df.drop(['id'],axis=1,inplace=True)
-# Hence id is a unique value for all the customers visited for the cancer test, so removig the ID column for the dataset
-
-# Checking for Missing Values
-for i in df:
-    x=df[i].unique()
-    print(x,i)
-
-# here in the Bare_nuclei Attribute we are having the missing data replaced with ?
-df['Bare_nuclei'].value_counts()
-
-# there are 16 missing values replaced with ? , so replacing this ? with a constant 
-df.replace('?',-9999,inplace=True)
-print('new shape',df.shape)
+Data_cancer =pd.read_csv("breast-cancer-wisconsin.data", header=None)
+Data_cancer.columns = ['id','Clump_thickness','Uniformity_of_cellsize','uniformity_of_cellshape','Marginal_adhesion','Single_epithelain_cellsize',
+                       'Bare_nuclei','Bland_chromatin','Nornal_Nucleli','Mitoses','Class']
+Data_cancer.drop(['id'],axis=1,inplace=True)
+Data_cancer.replace('?',-9999,inplace=True)
+Data_cancer['Bare_nuclei']=Data_cancer['Bare_nuclei'].astype(int)
 
 # =============================================================================
 # #Splitting Data to Train and test data
 # =============================================================================
-from collections import Counter
-df['Bare_nuclei']=df['Bare_nuclei'].astype(int)
+
 def split_train_valid_test(data,test_ratio): 
     shuffled_indcies=np.random.permutation(len(data)) 
     test_set_size= int(len(data)*test_ratio) 
     test_indcies=shuffled_indcies[:test_set_size] 
     train_indices=shuffled_indcies[test_set_size:] 
     return data.iloc[train_indices],data.iloc[test_indcies]
-train_set,test_set=split_train_valid_test(df,test_ratio=0.2)
-print('lenghth of Trainig set : ',len(train_set)) 
-print('lenghth of Testing set : ',len(test_set))
 
-# =============================================================================
-# # Custom KNN without SKlearn Model
-# =============================================================================
-
+train_set,test_set=split_train_valid_test(Data_cancer,test_ratio=0.2)
 X_train=np.array(train_set.drop(['Class'],axis =1))
 y_train=np.array(train_set['Class'])
 X_test=np.array(test_set.drop(['Class'],axis =1))
 y_test=np.array(test_set['Class'])
 
-class KNNClassifier(object):
+# =============================================================================
+# # Custom KNN without SKlearn Model
+# =============================================================================
+
+class KNNClassifier():
+
     def __init__(self):
         pass
-    
-    #"training" function
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
-
-    #predict function, output of this function is linked to main function to predict the testdata
     def predict(self, X_test, k=5):
         distances = self.compute_distances(self.X_train, X_test)
         vote_results = []
@@ -120,17 +62,13 @@ class KNNClassifier(object):
                 votesOneSample.append(distances[i][j][1])
             vote_results.append(Counter(votesOneSample).most_common(1)[0][0])
         return vote_results
-    
-    #For each sample and every item in test set algorithm is making tuple in distance list
-    #this is how list looks =>> distances = [[[distance, class],[distance, class],[distance, class],[distance, class]]]
-    #it will caluclate distances and sort them accordingly 
-     def compute_distances(self, X, X_test):
+    def compute_distances(self, X, X_test):
         distances = []
         for i in range(X_test.shape[0]):
             euclidian_distances = np.zeros(X.shape[0])
             oneSampleList = []
             for j in range(len(X)):
-                euclidian_distances[j] = np.linalg.norm(np.array(X_test[i])-np.array(X[j])) 
+                euclidian_distances[j] = np.sqrt(np.sum(np.square(np.array(X_test[i]) - np.array(X[j]))))
                 oneSampleList.append([euclidian_distances[j], self.y_train[j]])
             distances.append(sorted(oneSampleList))
         return distances
@@ -141,36 +79,52 @@ def accuracy(y_tes, y_pred):
         if(y_tes[i] == y_pred[i]):
             correct += 1
     return (correct/len(y_tes))*100
+
 def run():
     classifier = KNNClassifier()
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
     print("My KNN accuracy: ",accuracy(y_test, y_pred),'%')
+    y_actu = pd.Series(y_test, name='Actual')
+    y_pred = pd.Series(y_pred, name='Predicted')
+    df_confusion = pd.crosstab(y_actu, y_pred, rownames=['Actual'], colnames=['Predicted'], margins=True)
+    print('\n',df_confusion)
+    TN= df_confusion[2][2]
+    TP= df_confusion[4][4]
+    FP= df_confusion[4][2]
+    FN= df_confusion[2][4]
+    recall = TP /(TP+FN)
+    Precision = TP /(TP+FP)
+    Specificity = TN/(TN+FP)
+    F1score= 2 * ((Precision*recall) /(Precision + recall))
     
+    return {'Recall score':recall, 'Precision score':Precision,'Specificity':Specificity,'F1Score' :F1score}
+   
+
 start = time.time()
 run()
 end = time.time()
 time_taken = end - start
-print('Time Taken : ',time_taken)
+print('Time Taken for Custom KNN: ',time_taken)
+
 
 # =============================================================================
 # #Checking Accuracy and Time with KNN - SKlearn
 # =============================================================================
 
-df['newclass']=np.nan
-for i in range(len(df['Class'])):
-    if df['Class'].iloc[i] == 2:
-        df['newclass'].iloc[i] = 0 
+Data_cancer['newclass']=np.nan
+for i in range(len(Data_cancer['Class'])):
+    if Data_cancer['Class'].iloc[i] == 2:
+        Data_cancer['newclass'].iloc[i] = 0 
     else:
-        df['newclass'].iloc[i] = 1
+        Data_cancer['newclass'].iloc[i] = 1
 
 # changing in to binary class for getting better metrics
         
-df['newclass']=df['newclass'].astype('category')
-X=df.drop(['Class','newclass'],axis =1)
-y=df['newclass']
+Data_cancer['newclass']=Data_cancer['newclass'].astype('category')
+X=Data_cancer.drop(['Class','newclass'],axis =1)
+y=Data_cancer['newclass']
 
-from sklearn.model_selection import train_test_split
 X_train1, X_test1, y_train1, y_test1 = train_test_split(X,y, test_size=0.3, random_state=0)
 
 start = time.time()
@@ -181,49 +135,20 @@ print('With SKlEARN KNN (K=5) test accuracy is: ',knn.score(X_test1,y_test1))
 
 end = time.time()
 time_taken = end - start
-print('Time: ',time_taken)
+print('Time taken for SK learn: ',time_taken)
 
 # custom model is better at accuracy and lagging for time taking
 
-yy=pd.DataFrame(y_train)
-plt.rcParams['figure.figsize'] = (18, 7)
-plt.subplot(1, 2, 1)
-sns.countplot(yy[0], palette = 'pastel')
-plt.title('Training set : Benign or Malignant', fontsize = 30)
-plt.xlabel('class', fontsize = 15)
-plt.ylabel('count', fontsize = 15)
-plt.show()
-
-Percent_value_counts = pd.DataFrame(data={
-            'Count':yy[0].value_counts(),
-            '% of Total': (yy[0].value_counts()/(len(yy))*100)
-            }).sort_values('Count', ascending=False)
-Percent_value_counts
 # =============================================================================
 # # SMOTE MODEL for Balancing the data
 # =============================================================================
 
-from imblearn.over_sampling import SMOTE 
-print("Before OverSampling, counts of label '2': {}".format(sum(y_train==2)))
-print("Before OverSampling, counts of label '4': {} \n".format(sum(y_train==4)))
-
 sm = SMOTE(random_state=2)
 X_train_res, y_train_res = sm.fit_sample(X_train, y_train)
-print('After OverSampling, the shape of train_X: {}'.format(X_train_res.shape))
-print('After OverSampling, the shape of train_y: {} \n'.format(y_train_res.shape))
+
 print("After OverSampling, counts of label '2': {}".format(sum(y_train_res==2)))
 print("After OverSampling, counts of label '4': {}".format(sum(y_train_res==4)))
 
-yy1=pd.DataFrame(y_train_res)
-yy1.columns=['class']
-plt.rcParams['figure.figsize'] = (18, 7)
-plt.subplot(1, 2, 1)
-sns.countplot(yy1['class'], palette = 'pastel')
-plt.title('Training set : Benign or Malignant', fontsize = 30)
-plt.ylabel('count', fontsize = 15)
-plt.show()
-
-# Now Null Accuracy of the model is 50 % 
 # =============================================================================
 # # Custom KNN after Sampling
 # =============================================================================
@@ -232,97 +157,16 @@ def run():
     classifier = KNNClassifier()
     classifier.fit(X_train_res, y_train_res)
     y_pred1 = classifier.predict(X_test)
-    print("My KNN accuracy: ",accuracy(y_test, y_pred1),'%')
-    
-def run():
-    classifier = KNNClassifier()
-    classifier.fit(X_train_res, y_train_res)
-    y_pred1 = classifier.predict(X_test)
-    print ('\n******** Model Performance after Sampling******')
-    print("\n My KNN accuracy: ",accuracy(y_test, y_pred1),'%')
-    print("\nlength of y -test is :",len(y_test))
-    print("length of y -Pred is :",len(y_pred1))
-    y_actu = pd.Series(y_test, name='Actual')
-    y_pred = pd.Series(y_pred1, name='Predicted')
-    df_confusion = pd.crosstab(y_actu, y_pred, rownames=['Actual'], colnames=['Predicted'], margins=True)
-    print ('\n******** Model Metrics******')
-    print ('\n 1. Confusion Matrix')
-    print('\n',df_confusion)
-    TN= df_confusion[2][2]
-    TP= df_confusion[4][4]
-    FP= df_confusion[4][2]
-    FN= df_confusion[2][4]
-    
-    recall = TP /(TP+FN)
-    Precision = TP /(TP+FP)
-    Specificity = TN/(TN+FP)
-    F1score= 2 * ((Precision*recall) /(Precision + recall))
-    
-    print("\n 2.recall score is :",recall)
-    print("\n 3.Precision score is :",Precision)
-    print("\n 4.Specificity is :",Specificity)
-    print("\n 5. F1score is :",F1score)
-    
-start = time.time()
+    print("My KNN accuracy after smapling : ",accuracy(y_test, y_pred1),'%')
+
 run()
-end = time.time()
-time_taken = end - start
-print('Time Taken : ',time_taken)
-
-# =============================================================================
-# # SKlearn KNN ALGORITHM AFTER SAMPLING
-# =============================================================================
-print("Before OverSampling, counts of label '2' and newlabeled as 0: {}".format(sum(y_train1==0)))
-print("Before OverSampling, counts of label '4'  and newlabeled as 1: {} \n".format(sum(y_train1==1)))
-sm = SMOTE(random_state=2)
-X_train_res, y_train_res = sm.fit_sample(X_train1, y_train1)
-print('After OverSampling, the shape of train_X: {}'.format(X_train_res.shape))
-print('After OverSampling, the shape of train_y: {} \n'.format(y_train_res.shape))
-print("After OverSampling, counts of label '0': {}".format(sum(y_train_res==0)))
-print("After OverSampling, counts of label '1': {}".format(sum(y_train_res==1)))
-
-start = time.time()
-knn = KNeighborsClassifier(n_neighbors =5)
-knn.fit(X_train_res, y_train_res)
-y_pred = knn.predict(X_test1)
-print('With KNN (K=5) test accuracy is: ',knn.score(X_test1,y_test1))
-end = time.time()
-time_taken = end - start
-print('\n Time Taken : ',time_taken)
-
-# Finding Best K value for better Accuracy
-
-neig = np.arange(1, 25)
-test_accuracy = []
-# Loop over different values of k
-for i, k in enumerate(neig):
-    # k from 1 to 25(exclude)
-    knn = KNeighborsClassifier(n_neighbors=k)
-    # Fit with knn
-    knn.fit(X_train_res,y_train_res)
-    # test accuracy
-    test_accuracy.append(knn.score(X_test1, y_test1))
-
-# Plot
-import matplotlib.pyplot as plt
-plt.figure(figsize=[13,8])
-plt.plot(neig, test_accuracy, label = 'Testing Accuracy')
-plt.legend()
-plt.title('Value VS Accuracy',fontsize=20)
-plt.xlabel('Number of Neighbors',fontsize=20)
-plt.ylabel('Accuracy',fontsize=20)
-plt.xticks(neig)
-plt.grid()
-plt.show()
-print("Best accuracy is {} with K = {}".format(np.max(test_accuracy),1+test_accuracy.index(np.max(test_accuracy))))
-
-# So ,for Testing data K value is 4 and Best Accuracy is 97.61
 
 # Model Metrics
 mat_KNN = confusion_matrix(y_test1,y_pred)
 print("KNN model confusion matrix = \n",mat_KNN)
-mat_KNN = classification_report(y_test1,y_pred)
-print("KNN model confusion matrix = \n",mat_KNN)
+
+mat_KNN1 = classification_report(y_test1,y_pred)
+print("KNN model confusion matrix = \n",mat_KNN1)
 
 def draw_roc(actual,probs):
     fpr,tpr,thresholds = metrics.roc_curve(actual,probs)
@@ -337,21 +181,24 @@ def draw_roc(actual,probs):
     plt.title('ROC CURVE')
     plt.legend(loc="lower right")
     plt.show()
+    
     return fpr,tpr,thresholds
+
 draw_roc(y_test1,y_pred)
 
 #Changing the Threshold for Probablities
 
 y_pred_prob = knn.predict_proba(X_test1)[:, 1]
 y_pred_prob[1:20]
-# histogram of predicted probabilities
-plt.hist(y_pred_prob, bins=20)
 
-# x-axis limit from 0 to 1
+# histogram of predicted probabilities
+
+plt.hist(y_pred_prob, bins=20)
 plt.xlim(0,1)
 plt.title('Histogram of predicted probabilities for class 1')
 plt.xlabel('Predicted probability of Cancer Class')
 plt.ylabel('Frequency')
+
 
 ## Changing the cut off value for prediction
 pred_proba_df = pd.DataFrame(knn.predict_proba(X_test1))
